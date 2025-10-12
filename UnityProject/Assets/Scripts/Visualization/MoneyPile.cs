8ca1f41371c3d108c3c92240bc49d910cea074cd
ArrynOBrien <72053359+HatsuHodowa@users.noreply.github.com>
2025-10-11 21:53:57 -0400

    money pile demonstration

---
+++ UnityProject/Assets/Scripts/Visualization/MoneyPile.cs
@@ -0,0 +1,116 @@
+using NUnit.Framework.Constraints;
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MoneyPile : MonoBehaviour
+{
+	public float HeightPerDollar = 0.01f;
+	public GameObject Dollar;
+	public float MoneyAmount = 100;
+	public float spawnDelay = 0.1f;
+
+	private int currentAmount;
+	private int spawnedAmount;
+	private int spawnDollarSize;
+	private float lastSpawn = 0f;
+	private bool isSpawning = false;
+
+	private void Start()
+	{
+		InitializeMoneySpawn(MoneyAmount);
+	}
+
+	private void Update()
+	{
+		if (isSpawning)
+		{
+			// Checking spawn delay
+			float currentTime = Time.time;
+			float timePassed = currentTime - lastSpawn;
+			if (timePassed > spawnDelay)
+			{
+				// Spawning dollars
+				if (currentAmount >= spawnedAmount + spawnDollarSize)
+				{
+					SpawnDollar(spawnDollarSize);
+					spawnedAmount += spawnDollarSize;
+					lastSpawn = currentTime;
+				}
+				else
+				{
+					EndMoneySpawn();
+				}
+			}
+		}
+	}
+
+	private void InitializeMoneySpawn(float amount)
+	{
+		if (isSpawning)
+		{
+			throw new System.Exception("Cannot initialize money spawn while money spawn still active");
+		}
+
+		isSpawning = true;
+		currentAmount = (int) MathF.Floor(amount);
+		spawnDollarSize = GetDollarSize(amount);
+		spawnedAmount = 0;
+	}
+
+	private void EndMoneySpawn()
+	{
+		isSpawning = false;
+	}
+
+	private void InstantMoneySpawn(float amount, int dollarSize)
+	{
+		int stackAmount = (int) Mathf.Floor(amount / dollarSize);
+
+		for (int i = 0; i < stackAmount; i++)
+		{
+			SpawnDollar(dollarSize);
+		}
+	}
+
+	private void InstantMoneySpawn(float amount)
+	{
+		InstantMoneySpawn(amount, GetDollarSize(amount));
+	}
+
+	private void SpawnDollar(int size)
+	{
+		GameObject clone = Instantiate(Dollar);
+		clone.transform.parent = transform;
+		clone.transform.localPosition = Vector3.zero;
+		clone.transform.localScale = new Vector3(Dollar.transform.localScale.x, HeightPerDollar * size, Dollar.transform.localScale.z);
+	}
+
+	private int GetDollarSize(float amount)
+	{
+		if (amount <= 100)
+		{
+			return 1;
+		}
+		else if (amount <= 500)
+		{
+			return 5;
+		}
+		else if (amount <= 1000)
+		{
+			return 10;
+		}
+		else if (amount <= 2000)
+		{
+			return 20;
+		}
+		else if (amount <= 5000)
+		{
+			return 50;
+		}
+		else
+		{
+			return 100;
+		}
+	}
+}
